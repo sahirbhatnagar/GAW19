c506777ff02db60d444b1885cb9aca24bdf5e72a
sahirbhatnagar <sahir.bhatnagar@gmail.com>
2014-06-09 18:50:43 -0400

    add comments

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,3 +1,11 @@
+##################################
+# R source code file used to create variables used for GAW analysis
+# Created by Sahir, March 27, 2014
+# Updated March 27, 2014
+# hosted on Github repo 'sahirbhatnagar/gaw19'
+# NOTE: you need to change your working directory to where the data is
+##################################
+
 setwd("~/Documents/GAW19/")
 library(data.table)
 library(bit64)
