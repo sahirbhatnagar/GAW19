a6ae61dbd04f878f87ce8a91ec36879ae36652a5
sahirbhatnagar <sahir.bhatnagar@gmail.com>
2014-06-12 01:19:14 -0400

    genotype split functional

--- data_cleaning.R
+++ data_cleaning.R
@@ -49,9 +49,9 @@ write.table(DT.m, file=paste("trans",DT.m$chr[1],".tped",sep=""),col.names=FALSE
 read.fwf
 
 # Break up genotypes ------------------------------------------------------
-filename <- "chr21-geno.csv"
-DT <- fread(filename, select=2:5, nrows=10) #, nrows=200000)
-DT[,snp:=NULL]
+# filename <- "chr21-geno.csv"
+# DT <- fread(filename, select=2:5, nrows=10) #, nrows=200000)
+# DT[,snp:=NULL]
 #DT[,c("v1","v2"):=list(substr(DT$T2DG0200001, 1, 1),substr(DT$T2DG0200001, 2, 2))]
 
 #id <- colnames(DT)
@@ -59,28 +59,45 @@ DT[,snp:=NULL]
 
 #f <- function(x) {list(substr(x, 1, 1),substr(x, 2, 2))} 
 
-ids <- colnames(DT)
-gene.split <- function(i) 
-  {
-  as.data.table(do.call(rbind, strsplit(as.vector(eval(parse(text=paste("DT$",ids[i])))), split = "")))
-}
+# ids <- colnames(DT)
+# gene.split <- function(i) 
+#   {
+#   as.data.table(do.call(rbind, strsplit(as.vector(eval(parse(text=paste("DT$",ids[i])))), split = "")))
+# }
+# 
+# DT
+# all.gene <- foreach(i=1:length(ids)) %dopar% gene.split(i)
+# do.call(cbind,all.gene)
 
 
-
-# Break up genotypes NEW --------------------------------------------------
+# Break up genotypes NEW - This works --------------------------------------------------
 
 rm(DT)
 filename <- "chr21-geno.csv"
-DT <- fread(filename, select=2:100)#, nrows=10) #, nrows=200000)
+DT <- fread(filename)#, select=2:300)#, nrows=10) #, nrows=200000)
 out_names <- paste("V", 1:(2*ncol(DT)), sep="_")
 invar1 <- names(DT)
-
+alloc.col(DT,3000)
 for (i in seq_along(invar1)) {
-  set(DT, i=NULL, j=out_names[2*i-1], value=do.call(rbind, strsplit(DT[[invar1[i]]], split = ""))[,1])
-  set(DT, i=NULL, j=out_names[2*i], value=do.call(rbind, strsplit(DT[[invar1[i]]], split = ""))[,2])
+  set(DT, i=NULL, j=out_names[2*i-1], value=substr(DT[[invar1[i]]],1,1))
+  set(DT, i=NULL, j=out_names[2*i], value=substr(DT[[invar1[i]]],2,2))
 }
 
-DT[1:5,c(18,19,56:57),with=FALSE]
+
+
+# DT[1:25,c(1,2,299:305), with=FALSE]
+# for (i in seq_along(invar1)) {
+#   set(DT, i=NULL, j=out_names[2*i-1], value=do.call(rbind, strsplit(DT[[invar1[i]]], split = ""))[,1])
+#   set(DT, i=NULL, j=out_names[2*i], value=do.call(rbind, strsplit(DT[[invar1[i]]], split = ""))[,2])
+# }
+
+# DT[1:5,c(18,19,56:57),with=FALSE]
+
+
+# method 1 from SO
+# pairs <- c(outer(c('C', 'T', 'G', 'A'), c('C', 'T', 'G', 'A'), 'paste0'))
+# l <- replicate(1000, sample(pairs, 260000, replace=TRUE), simplify=FALSE)
+
 
 
 # PED file -------------------------------------------------------------
