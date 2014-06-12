70225bc320cf477d0ff3718e31481c7afe706685
sahirbhatnagar <sahir.bhatnagar@gmail.com>
2014-06-11 20:04:15 -0400

    two solutions to split genotypes still not working

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,7 +1,7 @@
 ##################################
 # R source code file used to create variables used for GAW analysis
 # Created by Sahir, March 27, 2014
-# Updated March 27, 2014
+# Updated June 10, 2014
 # hosted on Github repo 'sahirbhatnagar/gaw19'
 # NOTE: you need to change your working directory to where the data is
 ##################################
@@ -10,28 +10,84 @@ setwd("~/git_repositories/GAW19/data/")
 library(data.table)
 library(bit64)
 library(plotrix)
+library(stringr)
+library(doParallel)
+registerDoParallel(cores = 4)
+library(foreach)
 
 
 # Genotype Files ----------------------------------------------------------
-
+#needs to be in the following 4 column formate
+# chromosome number | snp identifier | genetic distance (cM) | base pair
+filename <- "chr9-geno.csv"
 filename <- "chr21-geno.csv"
-filename <- "chr21t.csv"
-DT.geno <- fread(filename)
-DT.geno.t <- as.data.table(t(DT.geno[,2:ncol(DT.geno), with=FALSE]), keep.rownames=TRUE)
-setnames(DT.geno.t,"rn","ID")
-setkey(DT.geno.t,ID)
+DT.geno <- fread(filename, select=1) #, nrows=200000)
+setkey(DT.geno, snp)
+
+#extract chromosome and base pair
+DT.geno$chr <- as.numeric(sub(" .*", "", gsub("\\_", " ", as.character(DT.geno$snp))))
+DT.geno$base <- as.numeric(sub(" ", "", sub(DT.geno$chr[1],"",gsub("\\_", " ", as.character(DT.geno$snp)),perl=TRUE)))
+DT.geno$cM <- 0
+DT.geno[1:5,1:4,with=FALSE]
+setcolorder(DT.geno, c("chr","snp","cM","base"))
+write.table(DT.geno, file=paste("chr",DT.geno$chr[1],".map",sep=""),col.names=FALSE,
+            row.names=FALSE, quote=FALSE)
+
+
+#bring in SNP data
+DT <- fread(filename, select=2)#, nrows=10) #, nrows=200000)
+people <- colnames(DT)[-1]
+setkey(DT,snp)
+
+DT.m <- DT[DT.geno]
+#DT.m[1:10,1:5,with=FALSE]
+setcolorder(DT.m, c("chr","snp","cM","base",people))
+rm(DT.geno,DT)
+write.table(DT.m, file=paste("trans",DT.m$chr[1],".tped",sep=""),col.names=FALSE,
+            row.names=FALSE, quote=FALSE)
+
+read.fwf
+
+# Break up genotypes ------------------------------------------------------
+filename <- "chr21-geno.csv"
+DT <- fread(filename, select=2:5, nrows=10) #, nrows=200000)
+DT[,snp:=NULL]
+#DT[,c("v1","v2"):=list(substr(DT$T2DG0200001, 1, 1),substr(DT$T2DG0200001, 2, 2))]
+
+#id <- colnames(DT)
+#ids <- unlist(lapply(id, function(i) rep(i,2)))
+
+#f <- function(x) {list(substr(x, 1, 1),substr(x, 2, 2))} 
+
+ids <- colnames(DT)
+gene.split <- function(i) 
+  {
+  as.data.table(do.call(rbind, strsplit(as.vector(eval(parse(text=paste("DT$",ids[i])))), split = "")))
+}
+
+
+
+# Break up genotypes NEW --------------------------------------------------
+
+rm(DT)
+filename <- "chr21-geno.csv"
+DT <- fread(filename, select=2:100)#, nrows=10) #, nrows=200000)
+out_names <- paste("V", 1:(2*ncol(DT)), sep="_")
+invar1 <- names(DT)
+
+for (i in seq_along(invar1)) {
+  set(DT, i=NULL, j=out_names[2*i-1], value=do.call(rbind, strsplit(DT[[invar1[i]]], split = ""))[,1])
+  set(DT, i=NULL, j=out_names[2*i], value=do.call(rbind, strsplit(DT[[invar1[i]]], split = ""))[,2])
+}
+
+DT[1:5,c(18,19,56:57),with=FALSE]
 
 
 # PED file -------------------------------------------------------------
 
+# leave this one as is, this is in the proper format as per
+# http://pngu.mgh.harvard.edu/~purcell/plink/data.shtml#tr 
 DT.ped <- fread("PED.csv")
-setkey(DT.ped,ID)
-tables()
+DT.ped
+write.table(DT.ped, file="trans.tfam",col.names=FALSE,row.names=FALSE, quote=FALSE)
 
-DT.all <- DT.ped[DT.geno.t]
-
-setcolorder(DT.all,c("PEDNUM","ID",colnames(DT.all)[-1:-2]))
-
-
-source("http://bioconductor.org/biocLite.R")
-biocLite("GWASTools")
