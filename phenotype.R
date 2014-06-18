16fb03cd324f38318e4e75a0cf07514f258a45fe
sahirbhatnagar <sahir.bhatnagar@gmail.com>
2014-06-17 21:12:01 -0400

    added manhattan plots

--- phenotype.R
+++ phenotype.R
@@ -32,15 +32,22 @@ f_dowle3 = function(DT) {
 #replace NA's with -9 which is default missing for plink phenotype files
 f_dowle3(DT.phen)
 
+# add phenotype where everyone is affected except missing
+set(DT.phen, which(DT.phen[["HTN_1"]]==-9), "EVERYONE_AFFECTED", value=-9)
+set(DT.phen, which(DT.phen[["HTN_1"]]!=-9), "EVERYONE_AFFECTED", value=1)
+
+# add phenotype where Cases --> Missing and Controls --> Cases
+# table(DT.phen$HTN_1);table(DT.phen$HTN_2)
+# table(DT.phen$HTN_3);table(DT.phen$HTN_4)
+set(DT.phen, which(DT.phen[["HTN_1"]]==1),"SWITCH_PHENO",value=-9)
+set(DT.phen, which(DT.phen[["HTN_1"]]==0),"SWITCH_PHENO",value=1)
+set(DT.phen, which(DT.phen[["HTN_1"]]==-9),"SWITCH_PHENO",value=-9)
+
 #write phenotype file
 write.table(DT.phen, file="pheno.txt", col.names=TRUE,
             row.names=FALSE, quote=FALSE)
 
-table(DT.phen[,"HTN_1",with=FALSE])
 
 
 
-# TDT results -------------------------------------------------------------
 
-DT.tdt <- as.data.table(read.table("plink.tdt", header=TRUE))
-str(DT.tdt)
