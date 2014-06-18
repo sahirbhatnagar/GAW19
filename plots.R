993b4377e1e5a1f7550c1ac8d58065d27a7a2969
sahirbhatnagar <sahir.bhatnagar@gmail.com>
2014-06-18 17:22:13 -0400

    hwe p value vs maf not giving expected results

--- plots.R
+++ plots.R
@@ -7,32 +7,72 @@
 ##################################
 
 setwd("~/git_repositories/GAW19/data/")
+rm(list=ls())
+#DT.frq[["CHR"]][1]
+#system(paste("mkdir", DT.frq[["CHR"]][1], sep=" "))
 
 DT.frq <- as.data.table(read.table("chr21.frq", header=TRUE))
-DT.frq[,hist(MAF)]
-
 DT.hwe <- as.data.table(read.table("chr21.hwe", header=TRUE))
-DT.hwe[1:5,1:9,with=FALSE]
+setkey(DT.hwe,SNP)
+#setkey(DT.hwe,NULL)
+setkey(DT.frq, SNP)
+DT.all <- DT.hwe[TEST=="ALL"][DT.frq]
+DT.all[MAF!=0][,plot(MAF,-log10(P))]
+#rm(DT.all)
 
-setkey(DT.hwe,TEST)
+DT.all <- cbind(DT.frq,DT.hwe["ALL"])
+DT.all[,log.expect.p:=-log10((1:nrow(DT.all)-0.5)/nrow(DT.all))]
+DT.all[,log.sorted.p:=-log10(sort(P))]
 
-#p-values
-DT.hwe["ALL"][,"P",with=FALSE]
+#data for founders only
+DT.frq.found <- as.data.table(read.table("founders.frq", header=TRUE))
+DT.hwe.found <- as.data.table(read.table("founders.hwe", header=TRUE))
+setkey(DT.hwe.found,SNP)
+setkey(DT.frq.found,SNP)
+DT.all.found <- DT.hwe.found[TEST=="ALL"][DT.frq.found]
+DT.all.found[MAF!=0][,plot(MAF,-log10(P))]
 
-DT.all <- cbind(DT.frq,DT.hwe["ALL"] )
+DT.all.found[,log10p:=-log10(P+1e-10)]
+DT.all.found[,plot(MAF,log10p)]
+
+
+plot(DT.all.found[,"log10p", with=FALSE])
+
+
+DT.all.found[,log.expect.p:=-log10((1:nrow(DT.all.found)-0.5)/nrow(DT.all.found))]
+DT.all.found[,log.sorted.p:=-log10(sort(P))]
+
+
+DT.tdt.every.affect <- as.data.table(read.table("everyone_affected.tdt", header=TRUE))
+setkey(DT.tdt.every.affect,SNP)
+DT.tdt.switch <- as.data.table(read.table("switch_pheno.tdt", header=TRUE))
+setkey(DT.tdt.switch,SNP)
+
+
+
+DT.frq[,hist(MAF)]
+DT.frq.found[,hist(MAF)]
+
+
 
 DT.all[,plot(MAF,-log10(P))]
+DT.all.found[,plot(MAF,-log10(P))]
 
 
 
-DT.all[,log.expect.p:=-log10((1:nrow(DT.all)-0.5)/nrow(DT.all))]
-DT.all[,sorted.p:=-log10(sort(P))]
-DT.all[,plot(log.expect.p,sorted.p)]
+DT.all[,plot(log.expect.p,log.sorted.p)]
+abline(a=0,b=1)
+DT.all.found[,plot(log.expect.p,log.sorted.p)]
 abline(a=0,b=1)
 
 
 
+manhattan(DT.tdt.every.affect)
+manhattan(DT.tdt.switch)
 
-# DT.all[,sorted.p:=NULL]
-# DT.all[,expect.p:=NULL]
-# DT.all[,log.expect.p:=NULL]
+
+
+qq(DT.tdt.every.affect$P, main="Q-Q plot of TDT p-values")
+qq(DT.tdt.switch$P, main="Q-Q plot of TDT p-values")
+
+
