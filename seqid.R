cb63170664745917e977d176c3c1a0cf3fdbf532
sahirbhatnagar <sahir.bhatnagar@gmail.com>
2014-06-25 00:44:40 -0400

    those who were sequenced

---
+++ seqid.R
@@ -0,0 +1,17 @@
+##################################
+# R source code file used to create ID's who were actually sequenced
+# Created by Sahir, June 24, 2014
+# Updated June 24, 2014
+# hosted on Github repo 'sahirbhatnagar/gaw19'
+# NOTE: you need to change your working directory to where the data is
+# I used pseq (shell script program) to extract ID of sequenced people from vcf files 
+##################################
+
+setwd("~/git_repositories/GAW19/data")
+DT <- fread("seq.id", header=F)
+str(DT)
+set(DT, i=NULL, j="FID", value=as.numeric(substr(DT[["V1"]],5,6)))
+setcolorder(DT, c("FID","V1"))
+
+write.table(DT, file="sequenced.id",
+            col.names=FALSE, row.names=FALSE, quote=FALSE)
